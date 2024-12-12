9db449d6b3e473ffc11faba664f84e6ece06095f
Brayan6072 <brayan.delgado6072@alumnos.udg.mx>
2024-12-11 18:11:27 -0600

    Campo duracion

--- js/Agregar.js
+++ js/Agregar.js
@@ -17,11 +17,12 @@ document.getElementById('formAgregar').addEventListener('submit', function(event
 
     const materia = document.getElementById('materia').value;
     const diaInput = document.getElementById('dia').value;
-    const hora = parseInt(document.getElementById('hora').value); 
+    let hora = parseInt(document.getElementById('hora').value); 
     const color = document.getElementById('colorPicker').value;
     const colortxt = document.getElementById('colortext').value;
     const maestro = document.getElementById('maestro').value;
     const nrc = document.getElementById('nrc').value;
+    const duracion = document.getElementById('duracion').value;
     let diaIndex = -1; 
 
     for (var i = 0; i < semana.length; i++) {        
@@ -40,15 +41,17 @@ document.getElementById('formAgregar').addEventListener('submit', function(event
     //const horario = document.querySelector('tbody tr:nth-child(' + hora + ') td:nth-child(' + diaIndex + ')');
 
     
+    //hora es la fila segun la hora, ejemplo 8:00-8:59 cuenta con el id 1 y las 20:00 cuenta con el id 13
+    //diaIndex es la columna segun el dia 1-7
+    const horario = document.getElementById(""+hora+diaIndex+"");//celda    
 
-    const horario = document.getElementById(""+hora+diaIndex+"");
-
-    const contentm = document.createElement('p');
-    const contentmaestro = document.createElement('p');
-    const contentnrc = document.createElement('p');
+    const contentm = document.createElement('p');//parrafo para mostrar el nombre de la materia
+    const contentmaestro = document.createElement('p');//parrafo para mostrar el nombre del maestro
+    const contentnrc = document.createElement('p');//parrafo para mostrar el nrc
     
-    contentm.textContent = materia;
-    contentm.style.color = colortxt;
+    //contentm - variable para materia
+    contentm.textContent = materia;//asigna el texto
+    contentm.style.color = colortxt;//asigna el color de la letra
 
     contentmaestro.textContent = maestro;
     contentmaestro.style.color = colortxt;
@@ -57,12 +60,12 @@ document.getElementById('formAgregar').addEventListener('submit', function(event
     contentnrc.style.color = colortxt;
     
     horario.innerHTML = ''; 
-
+    
     horario.appendChild(contentm);
     horario.appendChild(contentmaestro);
     horario.appendChild(contentnrc);
     
-    horario.style.backgroundColor = color;
+    horario.style.backgroundColor = color;//fondo de la celda
     
     
     const clase = {
@@ -74,6 +77,42 @@ document.getElementById('formAgregar').addEventListener('submit', function(event
     };
 
     localStorage.setItem('clase_' + hora + diaIndex, JSON.stringify(clase));
+
+    if(duracion == 2){
+        const horario2 = document.getElementById(""+hora+diaIndex+"");   
+        horario2.style.backgroundColor = color;
+        const clase2 = {            
+            color: color            
+        };
+        hora = hora + 1;
+        localStorage.setItem('clase_' + hora + diaIndex, JSON.stringify(clase2));
+    }else if(duracion == 3){            
+
+        //guardar el color de fondo en un item
+        const clase2 = {            
+            color: color
+        };
+        const clase3 = {            
+            color: color
+        };
+
+        hora++;
+        const horario2 = document.getElementById(""+hora+diaIndex+"");        
+        horario2.style.backgroundColor = color;
+        localStorage.setItem('clase_' + hora + diaIndex, JSON.stringify(clase2));
+
+        hora++;
+        const horario3  = document.getElementById(""+hora+diaIndex+"");  
+        horario3.style.backgroundColor = color;
+        localStorage.setItem('clase_' + hora + diaIndex, JSON.stringify(clase3));
+    }else if (duracion < 0|| duracion > 3){
+        console.error('Duracion no valida.');
+    }
+
+
+
+
+
     window.location.href = 'https://brayan6072.github.io/';
 });
 
